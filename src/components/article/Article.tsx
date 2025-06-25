63d0b861b56286541e1f902747eeeb63dcc1b6d5
stilooz <stilooz@yandex.ru>
2025-06-25 20:28:41 +0300

    test

--- src/components/article/Article.tsx
+++ src/components/article/Article.tsx
@@ -3,7 +3,7 @@ import clsx from 'clsx';
 // Сообщаем вебпаку, что этот файл использует это изображение.
 import plane from 'src/images/plane.png';
 import { Text } from 'src/ui/text';
-//ddd
+
 import styles from './Article.module.scss';
 
 export const Article = () => {
