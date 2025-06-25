1223865eadebe7635cf58a2cf4386a1f86850550
stilooz <stilooz@yandex.ru>
2025-06-25 22:31:57 +0300

    добавил сайдбар

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { Article } from './components/article/Article';
+import { ArrowButton } from './components/arrow-button/ArrowButton';
+import { ArticleParamsForm } from './components/sidebar/ArticleParamsForm';
 
 const defaultSettings = {
 	fontSize: '16px',
@@ -10,6 +12,29 @@ const defaultSettings = {
 
 export const App = () => {
 	const [articleSettings, setArticleSettings] = useState(defaultSettings);
+	const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+	const sidebarRef = useRef<HTMLDivElement | null>(null);
+
+	useEffect(() => {
+		const handleClickOutside = (event: MouseEvent) => {
+			if (
+				sidebarRef.current &&
+				!sidebarRef.current.contains(event.target as Node)
+			) {
+				setIsSidebarOpen(false);
+			}
+		};
+
+		if (isSidebarOpen) {
+			document.addEventListener('mousedown', handleClickOutside);
+		} else {
+			document.removeEventListener('mousedown', handleClickOutside);
+		}
+
+		return () => {
+			document.removeEventListener('mousedown', handleClickOutside);
+		};
+	}, [isSidebarOpen]);
 
 	return (
 		<div
@@ -21,6 +46,15 @@ export const App = () => {
 					'--container-width': articleSettings.containerWidth,
 				} as React.CSSProperties
 			}>
+			<ArrowButton
+				isOpen={isSidebarOpen}
+				onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+			/>
+			{isSidebarOpen && (
+				<div className='sidebar' ref={sidebarRef}>
+					<ArticleParamsForm />
+				</div>
+			)}
 			<Article />
 		</div>
 	);
